20783bc590761f277320b8fee1f837d9aed1a52d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-10 16:44:37 -0600

    Aadd basic JSX header and main section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className='header'>
+        <h1 className='page-title'>ISO 8601 Converter</h1>
       </header>
-    </div>
+      <section className='converter-container'>
+
+      </section>
+    </main>
   );
 }
 
