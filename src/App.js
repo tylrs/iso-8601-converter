c8ba8b19fbc3d61594005fd700e179392581874d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-11 14:41:34 -0600

    Add input JSX and timestamp state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [timestamp, setTimestamp] = useState('')
+
+  const handleChange = (e) => {
+
+  }
+
   return (
     <main>
       <header className='header'>
         <h1 className='page-title'>ISO 8601 Converter</h1>
       </header>
       <section className='converter-container'>
-
+        <section className='input-section'>
+          <label for='timestamp'>Enter Timestamp</label>
+          <input type='text' id='timestamp' value={timeStamp} onChange={(e) => handleChange(e)}></input>
+        </section>
       </section>
     </main>
   );
