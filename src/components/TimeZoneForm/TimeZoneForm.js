1a10a9b80127e41e19ed39e3d1b253cdecb4d116
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-15 17:20:53 -0600

    Add state to Timezone form

---
+++ src/components/TimeZoneForm/TimeZoneForm.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import './TimeZoneForm.css';
+
+const TimeZoneForm = () => {
+    const [city, setCity] = useState('America/Denver')
+
+    
+}
+
+export default TimeZoneForm;
