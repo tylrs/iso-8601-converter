dfa4bc78600fee4b4e2590fa69a752dac2f1c365
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-15 17:23:38 -0600

    Add basic form jsx

--- src/components/TimeZoneForm/TimeZoneForm.js
+++ src/components/TimeZoneForm/TimeZoneForm.js
@@ -4,7 +4,14 @@ import './TimeZoneForm.css';
 const TimeZoneForm = () => {
     const [city, setCity] = useState('America/Denver')
 
-    
+    return (
+        <form>
+            <label for='changeTimezone'>Choose Time zone to Convert to</label>
+            <select name='changeTimezone' id='changeTimezone'>
+                <option value='America/Denver'>America/Denver</option>
+            </select>
+        </form>
+    )
 }
 
 export default TimeZoneForm;
