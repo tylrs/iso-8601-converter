119bcd14f86f035e08397766886dec691b405f0e
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-16 17:13:45 -0600

    Add margin top to form label

--- src/components/TimeZoneForm/TimeZoneForm.js
+++ src/components/TimeZoneForm/TimeZoneForm.js
@@ -5,7 +5,7 @@ import convertedTimeZones from '../time-zone-list';
 const TimeZoneForm = ({setCity, city}) => {
 
     return (
-        <form>
+        <form className='timezone-form'>
             <label for='changeTimezone'>Choose Destination Time Zone:</label>
             <select name='changeTimezone' id='changeTimezone' value={city} onChange={(e) => setCity(e.target.value)}>
                 {convertedTimeZones}
