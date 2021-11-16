b25079ca4d8707a7561f4c88ad33fc729ef1ab11
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-16 17:06:19 -0600

    Change city in App on form change

--- src/components/TimeZoneForm/TimeZoneForm.js
+++ src/components/TimeZoneForm/TimeZoneForm.js
@@ -2,13 +2,12 @@ import React, {useState} from 'react';
 import './TimeZoneForm.css';
 import convertedTimeZones from '../time-zone-list';
 
-const TimeZoneForm = () => {
-    const [city, setCity] = useState('America/Denver')
+const TimeZoneForm = ({setCity}) => {
 
     return (
         <form>
             <label for='changeTimezone'>Choose Destination Time Zone:</label>
-            <select name='changeTimezone' id='changeTimezone'>
+            <select name='changeTimezone' id='changeTimezone' onChange={(e) => setCity(e.target.value)}>
                 {convertedTimeZones}
             </select>
         </form>
