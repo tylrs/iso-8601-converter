5932170f12cb676ec31619617f2f1fcbbe627b13
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-29 11:13:10 -0600

    Add timestamp diff JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,9 @@ import TimeZoneForm from '../TimeZoneForm/TimeZoneForm';
 
 const App = () => {
   const [timestamp, setTimestamp] = useState('')
+  const [diffTimestamp1, setDiffTimestamp1] = useState('')
+  const [diffTimestamp2, setDiffTimestamp2] = useState('')
+  const [timestampDiff, setTimestampDiff] = useState('')
   const [converted, setConverted] = useState('')
   const [city, setCity] = useState('America/Denver')
 
@@ -53,6 +56,34 @@ const App = () => {
           </section>
         }
         </section>
+        <section className='diff-section'>
+          <label className='diff-label-1' htmlFor='timestamp'>Enter Timestamp</label>
+          <input 
+            className='timestamp-input-1' 
+            type='text' 
+            id='timestamp' 
+            value={diffTimestamp1} 
+            onChange={(e) => handleChange(e)}
+            placeholder='ex: yyyy-mm-ddThh:mm:ssz'
+          />
+          <label className='diff-label-2' htmlFor='timestamp'>Enter Timestamp</label>
+          <input 
+            className='timestamp-input-2' 
+            type='text' 
+            id='timestamp' 
+            value={diffTimestamp2} 
+            onChange={(e) => handleChange(e)}
+            placeholder='ex: yyyy-mm-ddThh:mm:ssz'
+          />
+        {timestampDiff && 
+          <section className='diff-output-section'>
+            <h2 className='timezone-title'>Not sure</h2>
+            <div className='diff-wrapper'>
+              <p className='diff-timestamp'>{timestampDiff}</p>
+            </div>
+          </section>
+        }
+        </section>  
       </section>
     </main>
   );
