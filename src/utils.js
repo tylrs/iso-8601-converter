8d90f30621bf91b9a2379d00ba7536b7e1348e61
Taylor Galloway <gallowaytaylor@outlook.com>
2021-12-15 10:53:18 -0600

    Add custom outputs if timestamps are the same or incorrect

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,10 @@
-export const calculateDiff = (timestamp1, timestamp2) => {
-    
+export const calculateDiff = (timestamp1, timestamp2) => {   
     let millisecondsDiff = Math.abs(new Date(timestamp1)- new Date(timestamp2))
+    if (millisecondsDiff === 0) {
+        return 'No difference'
+    } else if (isNaN(millisecondsDiff)) {
+        return 'Please enter 2 ISO 8601 timestamps'
+    }
     const diff = {
         days: Math.floor(millisecondsDiff / (1000 * 60 * 60 * 24)),
         hours: Math.floor((millisecondsDiff / (1000 * 60 * 60)) % 24),
@@ -10,9 +14,7 @@ export const calculateDiff = (timestamp1, timestamp2) => {
     }
     console.log("milliseconds", millisecondsDiff, diff)
     return Object.keys(diff).reduce((acc, unit) => {
-        if (diff[unit]) {
-            acc += `${unit}: ${diff[unit]} `
-        }
+        if (diff[unit]) acc += `${diff[unit]} ${unit} `
         return acc
     }, '')
 }
